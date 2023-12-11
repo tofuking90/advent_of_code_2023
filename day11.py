21fe2c352331ac35cf97e4fdc3c96cccb1be81d0
tofuking90 <153166664+tofuking90@users.noreply.github.com>
2023-12-10 22:09:25 -0800

    Update day11.py

--- day11.py
+++ day11.py
@@ -1,22 +1,15 @@
 ##### Day 11 #####
-txt = []
-with open("day11.txt","r") as f:
-    for line in f:
-        txt.append(line.rstrip('\n'))
+mat = np.array([list(line.strip()) for line in open('day11.txt','r')])
+dotrows = [ind for line, ind in zip(mat, range(len(mat))) if all(x=='.' for x in line)]
+dotcols = [ind for ind in range(len(mat[0])) if all([x[ind]=='.' for x in mat])]
+poss = np.where(mat=='#')
 
-dotrows = [ind for line, ind in zip(txt, range(len(txt))) if line == '.'*len(txt)]
-dotcols = [ind for ind in range(len(txt[0])) if all([x[ind]=='.' for x in txt])]
+def expanded_dists(expansion):
+    dists = 0
+    for x1, y1, ind in zip(poss[0], poss[1],range(len(poss[0]))):
+        for x2, y2 in zip(poss[0][ind+1:], poss[1][ind+1:]):
+            dists += abs(x1-x2) + abs(y1-y2) + sum([z in dotrows for z in range(min(x1,x2),max(x1,x2))])*(expansion-1) + sum([z in dotcols for z in range(min(y1,y2),max(y1,y2))])*(expansion-1)
+    return dists
 
-poss = []
-for line, ind in zip(txt,range(len(txt))):
-    for n, ind2 in zip(line,range(len(line))):
-        if n == '#':
-            poss.append([ind,ind2])
-
-expansion = 1000000
-dists = 0
-for pair, ind in zip(poss,range(len(poss))):
-    for pair2 in poss[ind+1:]:
-        x1,y1 = pair
-        x2,y2 = pair2
-        dists += abs(x1-x2) + abs(y1-y2) + sum([z in dotrows for z in range(min(x1,x2),max(x1,x2))])*(expansion-1) + sum([z in dotcols for z in range(min(y1,y2),max(y1,y2))])*(expansion-1)
+expanded_dists(2)
+expanded_dists(1000000)
