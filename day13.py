cf1358c56a7172b66ddd3dd78733875d5df67023
tofuking90 <153166664+tofuking90@users.noreply.github.com>
2023-12-14 04:30:35 -0800

    Add files via upload

---
+++ day13.py
@@ -0,0 +1,22 @@
+##### Day 13 #####
+import numpy as np
+mats = []
+with open('day13.txt','r') as f:
+    mat = []
+    for line in f:
+        if line.strip() == '':
+            mats.append(mat)
+            mat = []
+        else:
+            mat.append(line.strip())
+    mats.append(mat)
+
+def h_mirror(mat,s): # 0 for a, 1 for b
+    n_rows = len(mat)
+    for mir in range(1,n_rows):
+        half_len = min(n_rows - mir, mir)
+        if sum([sum(mat[mir-x-1] != mat[mir+x]) for x in range(half_len)]) == s:
+            return mir
+    return 0
+
+sum([100*h_mirror(np.array([list(l) for l in mat]),1) + h_mirror(np.array([list(l) for l in mat]).transpose(),1) for mat in mats])
