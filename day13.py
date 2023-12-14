5d8394ea136e9ee630e414c2554c3d816c720921
tofuking90 <153166664+tofuking90@users.noreply.github.com>
2023-12-14 15:22:27 -0800

    Update day13.py

--- day13.py
+++ day13.py
@@ -10,4 +10,5 @@ def h_mirror(mat,s): # 0 for a, 1 for b
             return mir
     return 0
 
+sum([100*h_mirror(np.array([list(l) for l in mat]),0) + h_mirror(np.array([list(l) for l in mat]).transpose(),0) for mat in mats])
 sum([100*h_mirror(np.array([list(l) for l in mat]),1) + h_mirror(np.array([list(l) for l in mat]).transpose(),1) for mat in mats])
